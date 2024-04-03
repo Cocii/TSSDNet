a0e9fee19679390c6b1c77695d178b08ee3f4666
Cocii <124038885@qq.com>
2024-04-03 10:30:51 +0800

    init

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
